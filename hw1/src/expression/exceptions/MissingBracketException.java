259b90a0de32d1e423679b4c7cb418b4b6e1b026
Emil <emil2001garipov@gmail.com>
2020-03-02 16:41:21 +0300

    expressions hierarchy changed

--- hw1/src/expression/exceptions/MissingBracketException.java
+++ hw1/src/expression/exceptions/MissingBracketException.java
@@ -2,6 +2,6 @@ package expression.exceptions;
 
 public class MissingBracketException extends ParsingException {
     public MissingBracketException(String s, int pos) {
-        super("Missing (" + s + ") bracket", pos);
+        super("Missing '" + s + "' bracket", pos);
     }
 }
