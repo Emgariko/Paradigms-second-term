37565e75a639872b8e1a3c70e93693bcded94e66
Emil <emil2001garipov@gmail.com>
2020-04-30 01:52:32 +0300

    Linear matrix&vector

--- hw9/clojure/linear.clj
+++ hw9/clojure/linear.clj
@@ -1,12 +1,27 @@
 (defn vector-operation [operation args] (apply mapv operation args))
-(defn v+ [& args] (vector-operation + args))
+(defn
+  
+  v+ [& args] (vector-operation + args))
 (defn v- [& args] (vector-operation - args))
 (defn v* [& args] (vector-operation * args))
 
 (defn scalar [a b] (reduce + (mapv * a b)))
 (defn v*s [a & args] (mapv (partial * (apply * args)) a))
+(defn vect [& args]
+  (reduce (fn [a b] (vector
+                      (- (* (nth a 1) (nth b 2)) (* (nth b 1) (nth a 2)))
+                      (- (* (nth b 0) (nth a 2)) (* (nth a 0) (nth b 2)))
+                      (- (* (nth a 0) (nth b 1)) (* (nth b 0) (nth a 1)))
+                      )
+            )
+          args)
+  )
 
-
-;(print (v+ [1 0 0] [0 1 0] [0 0 1]))
-;(print (scalar [2 3 -1] [3 -1 2]))
-(print (v*s [1 2 3] 2 3))
+(defn matrix-operation [vector-op args] (apply mapv vector-op args))
+(defn m+ [& args] (matrix-operation v+ args))
+(defn m- [& args] (matrix-operation v- args))
+(defn transpose [m] (apply mapv vector m))
+(defn m*v [m v] (mapv (fn [x] (reduce + (v* x v))) m))
+(defn m*s [m & ss] (mapv (fn [x] (v*s x (apply * ss))) m))
+(defn m* [& args] (reduce (fn [a b] (mapv (fn [x y] (mapv * x y)) a b)) args))
+(defn m*m [& args] (reduce (fn [a b] (transpose (mapv (partial m*v a) (transpose b)))) args))
