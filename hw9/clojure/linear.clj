0434368688b81d6ae4d04ccc6b85819ba037993c
Emil <emil2001garipov@gmail.com>
2020-05-02 17:19:08 +0300

    Copy-Paste fixed

--- hw9/clojure/linear.clj
+++ hw9/clojure/linear.clj
@@ -1,16 +1,22 @@
 (defn vsize-equals? [args] (every? (fn [x] (== (count (first args)) (count x))) args))
-(defn msize-equals? [args] (and (vsize-equals? args)))
+(defn msize-equals? [args] (vsize-equals? args))
 (defn isVector? [v] (and (every? number? v) (vector? v)))
 (defn isMatrix? [m] (and (vector? m) (every? isVector? m) (vsize-equals? m)))
+(defn create-operation [isType? size-equals?]
+  (fn [operation]
+    (fn [& args] {:pre [(every? isType? args) (size-equals? args)] :post [(isType? %)]} (apply mapv operation args))
+    ))
 
-(defn vector-operation [operation args] {:pre [(vsize-equals? args)]} (apply mapv operation args))
-(defn v+ [& args]
-  (vector-operation + args))
-(defn v- [& args] (vector-operation - args))
-(defn v* [& args] (vector-operation * args))
+(def vector-operation (create-operation isVector? vsize-equals?))
+(def matrix-operation (create-operation isMatrix? msize-equals?))
 
-(defn scalar [& args]  {:pre [(vsize-equals? args) (== (count args) 2)]} (reduce + (apply mapv * args)))
-(defn v*s [a & args] (mapv (partial * (apply * args)) a))
+(def v+ (vector-operation +))
+(def v- (vector-operation -))
+(def v* (vector-operation *))
+
+(defn scalar [& args] {:pre [(every? isVector? args) (vsize-equals? args) (== (count args) 2)]}
+  (reduce + (apply mapv * args)))
+(defn v*s [a & args] {:pre [(isVector? a) (every? number? args)]} (mapv (partial * (apply * args)) a))
 (defn vect [& args]
   {:pre [(vsize-equals? args)]}
   (reduce (fn [a b] (vector
@@ -22,12 +28,12 @@
           args)
   )
 
-(defn matrix-operation [vector-op args] (apply mapv vector-op args))
-(defn m+ [& args] (matrix-operation v+ args))
-(defn m- [& args] (matrix-operation v- args))
+
+(def m+ (matrix-operation v+))
+(def m* (matrix-operation v*))
+(def m- (matrix-operation v-))
 (defn transpose [m] (apply mapv vector m))
-(defn m*v [m v] (mapv (fn [x] (reduce + (v* x v))) m))
-(defn m*s [m & ss] (mapv (fn [x] (v*s x (apply * ss))) m))
-(defn m* [& args] {:pre [(msize-equals? args)]} (reduce (fn [a b] (mapv (fn [x y] (mapv * x y)) a b)) args))
-(defn m*m [& args] (reduce (fn [a b] (transpose (mapv (partial m*v a) (transpose b)))) args))
-;(println (m* (vector (vector 1.1 2.1 3.1) (vector 4.1 5.1 6.1)) (transpose (vector (vector 7.1 8.1 9.1) (vector 0.1 1.1 2.1)))))
+(defn m*v [m v] {:pre [(isVector? v) (isMatrix? m)]} (mapv (fn [x] (reduce + (v* x v))) m))
+(defn m*s [m & ss] {:pre [(isMatrix? m) (every? number? ss)]} (mapv (fn [x] (v*s x (apply * ss))) m))
+(defn m*m [& args] {:pre [(every? isMatrix? args)]}
+  (reduce (fn [a b] (transpose (mapv (partial m*v a) (transpose b)))) args))
