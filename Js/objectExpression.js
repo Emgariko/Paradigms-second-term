9a37c2649a9a11c1a248890a2bcaadf8bbce0060
Emil <emil2001garipov@gmail.com>
2020-05-14 01:33:53 +0300

    New parser + errors + bug fixed

--- Js/objectExpression.js
+++ Js/objectExpression.js
@@ -1,7 +1,9 @@
 "use strict";
+// review
 
 // :NOTE: `Variable op (0 args)     : org.graalvm.polyglot.PolyglotException: ExpressionError: Invalid operation format` where exactly
 // Non user-friendly error messages
+// fixed
 
 function Expr(evaluate, diff, toString, prefix, postfix) {
     this.prototype.evaluate = evaluate;
@@ -17,7 +19,6 @@ function setMethods(object, evaluate, diff, toString, prefix = toString, postfix
 
 //:NOTE: Const and Variable should be declared in the same way as other expressions (const Const = someFactory(...))
 // fixed
-
 function CreateConstOrVariable(constructor, get, diff, toString, prefix = toString, postfix = toString) {
     let result = constructor;
     setMethods(result, get, diff, toString);
@@ -27,9 +28,7 @@ const Const = CreateConstOrVariable(
     function(x) { this.x = x; },
     function() { return this.x },
     function() { return Const.ZERO },
-    function() { return this.x.toString() }/*,
-    function () { return this.x.toString() },
-    function () { return this.x.toString() }*/
+    function() { return this.x.toString() }
 )
 Const.ZERO = new Const(0);
 Const.ONE = new Const(1);
@@ -37,15 +36,14 @@ const Variable = CreateConstOrVariable(
     function(name) { this.name = name; this.argIndex = vars[name];},
     function(...args) { return args[this.argIndex] },
     function(variable) { return this.name === variable ? Const.ONE : Const.ZERO},
-    function() { return this.name.toString() }/*,
-    function() { return this.name.toString() },
-    function() { return this.name.toString() }*/
+    function() { return this.name.toString() }
 )
 const vars = {"x" : 0, "y" : 1, "z" : 2};
-let tokenToOperation = {};/*
-function AbsractToSpecialFormat(format, brackets) { // it's to fix copy-paste for 'toString', 'prefix', 'postfix'
-    return function() { }
-}*/ //:TODO: fix it
+let tokenToOperation = {};
+
+function toSpecialFormat(left, format, right, operands) {
+    return left + operands.map(format).join(' ') + right;
+}
 function CreateOperation(makeOperation, makeDiff, operationSymbol) {
     let operation = function (...args) {
         this.operands = args;
@@ -58,17 +56,10 @@ function CreateOperation(makeOperation, makeDiff, operationSymbol) {
             return this.makeDiff(variable, ...this.operands)
         },
         // :NOTE: copy-pasted code for `toString`, `prefix`, `postfix` (it can be described with one method with special parameters)
-        // :TODO:
-
-        function() {
-            return this.operands.join(' ') + ' ' + this.operationSymbol;
-        },
-        function() {
-            return '(' + this.operationSymbol + ' ' + this.operands.map(x => x.prefix()).join(' ') + ')';
-        },
-        function() {
-            return '(' + this.operands.map(x => x.postfix()).join(' ') + ' ' + this.operationSymbol + ')';
-        }
+        // fixed
+        function () { return toSpecialFormat('', (x) => (x.toString()), this.operationSymbol, this.operands) },
+        function () { return toSpecialFormat('(' + this.operationSymbol + ' ', (x) => (x.prefix()), ')', this.operands) },
+        function () { return toSpecialFormat('(', (x) => (x.postfix()), ' ' + this.operationSymbol + ')', this.operands) }
     );
     operation.prototype.makeOperation = makeOperation;
     operation.prototype.makeDiff = makeDiff;
@@ -143,7 +134,6 @@ const buildSum = function(...args) {
     for (let i = 2; i < args.length; i++) {
         res = new Add(res, args[i]);
     }
-    //console.log(...args);
     return res;
 }
 
@@ -237,22 +227,7 @@ function CreateStringSource() {
     source.prototype.getCurToken = function(convert) {
         return convert ? this.handle(this.curToken) : this.curToken;
     }
-    source.prototype.tokenCond = {
-        "Lb" : (token) => (token === '('),
-        "Rb" : (token) => (token === ')'),
-        "Const" : (token) => (/^-{0,1}\d+$/.test(token)),
-        "Operation" : (token) => (token in tokenToOperation),
-        "Variable" : (token) => (token in vars),
-        "End" : (token) => (token === '\0'),
-        "Empty" : (token) => (token === "")
-    }
     source.prototype.handle = function(token) {
-        //(this.tokenCond.entries()).forEac
-        /*for (let cur in this.tokenCond) {)
-            if (this.tokenCond[cur](token)) {
-                return cur;
-            }
-        }*/
         if (token === "(") {
             return "Lb";
         } else if (token === ")") {
@@ -278,15 +253,20 @@ const StringSource = CreateStringSource();
 
 //:NOTE: when have the rules changed? Why it's not wrapped into some factory method?
 //fixed
-//:TODO: create new factory-method
-function ExpressionError(message) {
-    this.message = message;
+function CreateAbstractError() {
+    let AbstractError = function(message) {
+        this.message = message;
+    }
+    AbstractError.prototype = Object.create(Error.prototype);
+    AbstractError.prototype.constructor = AbstractError;
+    AbstractError.prototype.name = "AbstractError";
+    return AbstractError;
 }
-ExpressionError.prototype = Object.create(Error.prototype);
-ExpressionError.constructor = ExpressionError;
+
+const ExpressionError = CreateAbstractError();
 function CreateExpressionError(name, head) {
     let error = function(source, message = '') {
-        ExpressionError.call(this, head + (message === '' ? "" : "")  + message + ', at pos ' + source.getPos() + ": " + source.getSubstr());
+        ExpressionError.call(this, head + ' ' + (message === '' ? "" : "")  + message + ', at pos ' + source.getPos() + ": " + source.getSubstr());
     }
     error.prototype = Object.create(ExpressionError.prototype);
     error.prototype.constructor = ExpressionError;
@@ -296,20 +276,22 @@ function CreateExpressionError(name, head) {
 
 const UnknownSymbolError = CreateExpressionError("UnknownSymbolError", "Unknown symbol");
 const UnexpectedTokenError = CreateExpressionError("UnexpectedTokenError", "Unexpected token");
-const UnexpectedRBracketError = CreateExpressionError("UnexpectedRBracketError", "Unknown right bracket");
 const MissingBracketError = CreateExpressionError("MissingBracketError", "Expected bracket");
 const UnexpectedEndError =  CreateExpressionError("UnknownEndError", "Unexpected end of source");
 const InvalidOperationFormatError = CreateExpressionError("InvalidOperationFormatError", "Invalid operation format");
 
+
+// :NOTE: too many code for parser. The limit is 50-60 non-blank lines
+// fixed
 function CreateParser() {
     let Parser = function(source) {
         this.source = source;
     }
-    Parser.prototype.test = function(condition, error, message) {
-        if (condition) { throw new error(this.source, message);}
+    Parser.prototype.test = function(condition, error, message = '') {
+        if (condition) { throw (message === '' ? new error(this.source) : new error(this.source, message)) }
     }
     Parser.prototype.throwError = function(error, message) {
-        throw new error(this.source, message);
+        throw (message === '' ? new error(this.source) : new error(this.source, message))
     }
     let _mode = undefined;
     Parser.prototype.parse = function(mode) {
@@ -340,11 +322,11 @@ function CreateParser() {
                 }
             }
             this.test(this.source.getCurToken(true) !== "Rb", MissingBracketError, "')'");
-            this.test(operationCounter !== 1, InvalidOperationFormatError);
+            this.test(operationCounter !== 1, InvalidOperationFormatError, "(operation expected)");
             this.test(_mode === "prefix" && pos !== 0, InvalidOperationFormatError, "for prefix mode");
             this.test(_mode === "postfix" && pos !== content.length - 1, InvalidOperationFormatError, "for postfix mode");
             let x = tokenToOperation[content[pos]].prototype.makeOperation.length;
-            this.test(x !== 0 && content.length - 1 !== x, InvalidOperationFormatError, ", incorrect count of args");
+            this.test(x !== 0 && content.length - 1 !== x, InvalidOperationFormatError, "(incorrect count of args)");
             return new tokenToOperation[content[pos]](...content.slice((_mode === "prefix" ? 1 : 0), content.length - 1 - (_mode === "postfix" ? 1 : 0) + 1));
         } else if (token === "Const") {
             return new Const(+this.source.getCurToken(false));
@@ -358,193 +340,11 @@ function CreateParser() {
 }
 
 const ExpressionParser = CreateParser();
-
 function parsePrefix(s) {
     let parser = new ExpressionParser(new StringSource(s.trim()));
     return parser.parse("prefix");
 }
-
 function parsePostfix(s) {
     let parser = new ExpressionParser(new StringSource(s.trim()));
     return parser.parse("postfix");
 }
-//console.log(parsePrefix("(negate)"));
-//console.log(parsePostfix("(x 2 +)"));
-//console.log(parsePostfix("(x 2 +)"));
-//console.log(parsePostfix("( sumexp)"));
-/*const s = new StringSource("zalupa");
-console.log(Object.entries(s.tokenCond).forEach());*/
-/*const Parser = CreateParser();
-const p = new Parser();
-p.parse("x");*/
-
-// :NOTE: too many code for parser. The limit is 50-60 non-blank lines
-// :TODO:
-
-/*
-function Parser(source) {
-    this.source = source;
-}
-Parser.prototype.isWhiteSpace = function(value) {
-    return /\s/.test(value);
-}
-Parser.prototype.isNumber = function(value) {
-    return /^-{0,1}\d+$/.test(value);
-}
-Parser.prototype.isOperation = function(value) {
-    return value in tokenToOperation;
-}
-Parser.prototype.isWord = function(value) {
-    return /^[0-9a-zA-Z]+$/.test(value);
-}
-Parser.prototype.tokenType = function(token) {
-    if (token === '(') {
-        return "Lb";
-    } else if (token === ')') {
-        return "Rb";
-    } else if (this.isNumber(token)) {
-        return "Const";
-    } else if (token in tokenToOperation) {
-        return "Operation";
-    } else if (token in vars) {
-        return "Variable";
-    } else if (token === '\0') {
-        return "End";
-    } else if (token === "") {
-        return "Empty";
-    } else {
-        // try to replace throw out of here
-        //throw new ExpressionError("Unknown symbol", this.source);
-        throw new UnknownSymbolError(this.source);
-        //throw new ExpressionError("Unknown or unexpected symbol");
-    }
-    /!*if (token === '\0') {
-        throw new ExpressionError("Unexpected end of source");
-    } else {
-
-    }*!/
-}
-
-Parser.prototype.parseGlobal = "";
-/!*Parser.prototype.parse = function(mode) {
-    return this.prototype.parseGlobal(mode);
-}*!/
-Parser.prototype.parse = function(mode) {
-    let token = this.parseToken();
-    let res;
-    if (this.tokenType(token) === "Lb") {
-        res = this.parseExpression(mode);
-    } else {
-        if (this.tokenType(token) === 'Variable') {
-            res = new Variable(token);
-        } else if (this.tokenType(token) === 'Const') {
-            res = new Const(+token);
-        } else if (this.tokenType(token) === "Rb") {
-            throw new UnexpectedRBracketError(this.source);
-            //throw new ExpressionError("Unexpected ) bracket", this.source);
-        } else if (this.tokenType(token) === "Operation") {
-            throw new MissingBracketError(this.source);
-            //throw new ExpressionError("Expected Bracket before " + token + " operation", this.source);
-        } else if (this.tokenType(token) === "End") {
-            throw new UnexpectedEndError(this.source);
-            //throw new ExpressionError("Unexpected end of source", this.source);
-        }
-    }
-    if (this.source.hasNext()) {
-        token = this.parseToken();
-        //throw new ExpressionError("Unexpected symbol: " + token[0]);
-        throw new MissingBracketError(this.source, "'('");
-        //throw new ExpressionError("Expected ( bracket", this.source);
-    }
-    return res;
-}
-Parser.prototype.parseExpression = function(mode) {
-    let content = [];
-    let token;
-    let operationCounter = 0, operationId = -1;
-    while (true) {
-        token = this.parseToken();
-        let type = this.tokenType(token);
-        switch (type) {
-            case 'Lb':
-                content.push(this.parseExpression(mode));
-                break;
-            case 'Const':
-                content.push(new Const(+token));
-                break;
-            case 'Variable':
-                content.push(new Variable(token));
-                break;
-            case 'Operation':
-                content.push(token);
-                operationCounter++;
-                operationId = content.length - 1;
-                break;
-            case 'End':
-                throw new MissingBracketError(this.source, "')'");
-                //throw new ExpressionError("Missing ) bracket", this.source);
-            case 'Rb':
-                break;
-        }
-        if (type === 'Rb') {
-            break;
-        }
-    }
-    if (operationCounter > 1) {
-        throw new MissingBracketError(this.source, "'('");
-        //throw new ExpressionError("Missing ( bracket", this.source);
-        //throw new ExpressionError("Invalid operation format");
-    }
-    if (mode === "prefix" && operationId != 0) {
-        throw new InvalidOperationFormatError(this.source);
-        //throw new ExpressionError("Invalid operation format", this.source);
-    }
-    if (mode === "postfix" && operationId != content.length - 1) {
-        throw new InvalidOperationFormatError(this.source);
-        //throw new ExpressionError("Invalid operation format", this.source);
-    }
-    let x = tokenToOperation[content[operationId]].prototype.makeOperation.length;
-    if (x !== 0 && content.length - 1 !== x) {
-        throw new InvalidOperationFormatError(this.source, "too many arguments for operation " + content[operationId]);
-        //throw new ExpressionError("Too many arguments for operation " + content[operationId], this.source);
-    }
-    let l = 0, r = content.length - 1;
-    if (mode === "prefix") {
-        l++;
-    } else if (mode === "postfix") {
-        r--;
-    }
-    return new tokenToOperation[content[operationId]](...content.slice(l, r + 1));
-}
-Parser.prototype.skipWhiteSpaces = function() {
-    while (this.isWhiteSpace(this.source.cur())) {
-        this.source.next();
-    }
-}
-Parser.prototype.parseToken = function() {
-    this.skipWhiteSpaces();
-    if (this.source.check('(') || this.source.check(')')) {
-        return this.source.next();
-    }
-    let token = '';
-    while (this.source.hasNext() && !this.isWhiteSpace(this.source.cur()) && !this.source.check('(') && !this.source.check(')')) {
-        token = token + this.source.cur();
-        this.source.next();
-    }
-    token = (token === '') ? this.source.next() : token;
-    return token;
-}
-
-function ExpressionParser(source) {
-    Parser.call(this, source);
-}
-ExpressionParser.prototype = Object.create(Parser.prototype);
-function parsePrefix(s) {
-    let parser = new ExpressionParser(new StringSource(s.trim()));
-    return parser.parse("prefix");
-}
-
-function parsePostfix(s) {
-    let parser = new ExpressionParser(new StringSource(s.trim()));
-    return parser.parse("postfix");
-}*/
